30b8ab42e66620327cfdfe638a47d1d695b6fd53
syukur.musa <syukur.musa@cloud-connect.asia>
2024-05-22 20:01:33 +0800

    change code

--- src/index.py
+++ src/index.py
@@ -8,4 +8,6 @@ app = Flask(__name__)
 def index():
 	return render_template("Mesin.html")
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port =5000)
+	from waitress import serve
+	serve(app,host='0.0.0.0',port=5000)
+	#app.run(host='0.0.0.0', port =5000)
