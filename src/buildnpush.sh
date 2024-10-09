80c7f7391baaeb43129aec94b6d632dc2b1e1586
syukur.musa <syukur.musa@cloud-connect.asia>
2024-10-10 00:22:38 +0800

    edit buider autoremove

--- src/buildnpush.sh
+++ src/buildnpush.sh
@@ -1,3 +1,2 @@
 docker rmi syukurdocker/water_dispenser:latest
-docker buildx create --rm --use --platform=linux/arm64,linux/amd64 --name multi-platform-builder
-docker buildx build --platform linux/amd64,linux/arm64 --push -t syukurdocker/water_dispenser .
+docker buildx build --platform=linux/arm64,linux/amd64 --builder=$(docker buildx create --use) --push -t syukurdocker/water_dispenser .
